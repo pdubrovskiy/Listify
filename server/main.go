6f3423f987f268bcf663bb54ff639716f1bb6f3a
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2025-02-08 19:51:56 +0300

    L-1: package for env added

--- server/main.go
+++ server/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/pdubrovskiy/listify/routes"
 )
 
 func main() {
 	app := fiber.New()
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+
 	routes.SetupTodoRoutes(app)
 
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(":" + PORT))
 }
